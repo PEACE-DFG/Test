a7022efa9ded5fdd876d773fa11f1cc6c7bc36fb
kanyin01 <kanyinsolaarowosegbe@gmail.com>
2023-07-24 17:00:36 +0100

    phase1

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
 const continueBtn = document.querySelector('.continue-btn');
 const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -19,4 +20,5 @@ continueBtn.onclick = () => {
     quizSection.classList.add('active');
     popupInfo.classList.remove('active');
     main.classList.remove('active');
+    quizBox.classList.add('active');
 }
